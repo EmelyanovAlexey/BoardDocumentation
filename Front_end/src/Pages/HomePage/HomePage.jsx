32a6168a6f74224e4317f5c1726b979644aa4d55
Alex <43723067+EmelyanovAlexey@users.noreply.github.com>
2024-11-10 20:22:30 +0700

    основная страница

--- Front_end/src/Pages/HomePage/HomePage.jsx
+++ Front_end/src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import { COLOR_LAYER } from '../../Shered/const';
+
+import LightBar from '../../Components/LightBar';
+import Panel from '../../Components/Panel';
+import ColorPickerСircle from '../../Components/ColorPickerСircle';
+
 import styles from './HomePage.module.css';
 
 function HomePage() {
+  const [selectedColor, setSelectedColor] = useState('red');
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -42,6 +51,54 @@ function HomePage() {
               ВИРТУАЛЬНАЯ ИНТЕРАКТИВНАЯ ДОСКА
             </h1>
           </div>
+        </div>
+      </div>
+
+      <div className={clsx([styles.section, styles.top_shadow])}>
+        <img
+          className={styles.fon}
+          src="../../../Img/fon_doc_1.png"
+          alt="fon_doc_1"
+        />
+
+        <div className={styles.section_conent}>
+          <div className={styles.title}>Распознование лазеров</div>
+
+          <div className={styles.color_picker}>
+            <ColorPickerСircle
+              list={COLOR_LAYER}
+              selected={selectedColor}
+              onClick={(color) => setSelectedColor(color.id)}
+            />
+            <LightBar
+              className={clsx([styles.layer, styles.layer_1])}
+              color={selectedColor}
+            />
+            <LightBar
+              className={clsx([styles.layer, styles.layer_2])}
+              color={selectedColor}
+            />
+            <LightBar
+              className={clsx([styles.layer, styles.layer_3])}
+              color={selectedColor}
+            />
+            <LightBar
+              className={clsx([styles.layer, styles.layer_4])}
+              color={selectedColor}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={clsx([styles.section, styles.top_shadow])}>
+        <img
+          className={styles.fon}
+          src="../../../Img/fon_doc_1.png"
+          alt="fon_doc_1"
+        />
+
+        <div className={styles.section_conent}>
+          <div className={styles.title}>Экономичный продукт</div>
         </div>
       </div>
     </div>
